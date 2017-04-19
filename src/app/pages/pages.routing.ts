b5ede73f10a23c4e82b039302574384d8565cb84
augustine <ashramwen@gmail.com>
2017-04-19 15:52:53 +0800

    remove register

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,4 +1,4 @@
-import { RouterModule, Routes }  from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { ModuleWithProviders } from '@angular/core';
 import { Pages } from './pages.component';
@@ -7,24 +7,17 @@ import { Pages } from './pages.component';
 
 // export function loadChildren(path) { return System.import(path); };
 
-export const routes: Routes = [
-  {
-    path: 'login',
-    loadChildren: 'app/pages/login/login.module#LoginModule'
-  },
-  {
-    path: 'register',
-    loadChildren: 'app/pages/register/register.module#RegisterModule'
-  },
-  {
-    path: 'pages',
-    component: Pages,
-    children: [
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-      { path: 'dashboard', loadChildren: 'app/pages/dashboard/dashboard.module#DashboardModule' },
-      { path: 'tables', loadChildren: 'app/pages/tables/tables.module#TablesModule' }
-    ]
-  }
-];
+export const routes: Routes = [{
+  path: 'login',
+  loadChildren: 'app/pages/login/login.module#LoginModule'
+}, {
+  path: 'pages',
+  component: Pages,
+  children: [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard', loadChildren: 'app/pages/dashboard/dashboard.module#DashboardModule' },
+    { path: 'tables', loadChildren: 'app/pages/tables/tables.module#TablesModule' }
+  ]
+}];
 
 export const routing: ModuleWithProviders = RouterModule.forChild(routes);
