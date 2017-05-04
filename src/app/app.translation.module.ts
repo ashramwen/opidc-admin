f9da106c5ee4f9ebc5919dc5ad82b767892c46a0
augustine <ashramwen@gmail.com>
2017-05-04 17:33:43 +0800

    set lang once

--- src/app/app.translation.module.ts
+++ src/app/app.translation.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { Http, HttpModule } from '@angular/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
-import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
-import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { NgModule } from '@angular/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateService } from '@ngx-translate/core';
 
 export function createTranslateLoader(http: Http) {
-    return new TranslateHttpLoader(http, './assets/i18n/US/', '.json');
+  return new TranslateHttpLoader(http, './assets/i18n/US/', '.json');
 }
 
 const translationOptions = {
@@ -24,8 +24,8 @@ const translationOptions = {
 })
 export class AppTranslationModule {
   constructor(private translate: TranslateService) {
-    translate.addLangs(["en"]);
+    translate.addLangs(['en']);
     translate.setDefaultLang('en');
-    translate.use('en');
+    // translate.use('en');
   }
 }
